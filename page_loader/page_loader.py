c2a372d01c65b54e7fd569634e33a1d4d30fa3ab
Евгений Назиров <eugenenazirov@MacBook-Air-Evgenij.local>
2022-08-15 22:24:56 +0600

    Update make_file_name func

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -11,19 +11,18 @@ import os
 # print(path)
 
 def make_file_name(link: str):
-    if link.startswith('https://') or link.startswith('http://'):
-        parse_link = link.split('/')
-        parse_link.pop(0)
-        parse_link.pop(0)
-        parse_link_to_str = '-'.join(parse_link)
-        spec_sym_to_dash = ''
-        for char in parse_link_to_str:
-            if not char.isalnum():
-                spec_sym_to_dash += '-'
-            else:
-                spec_sym_to_dash += char
-        result = spec_sym_to_dash + '.html'
-        return result
+    parse_link = link.split('/')
+    parse_link.pop(0)
+    parse_link.pop(0)
+    parse_link_to_str = '-'.join(parse_link)
+    spec_sym_to_dash = ''
+    for char in parse_link_to_str:
+        if not char.isalnum():
+            spec_sym_to_dash += '-'
+        else:
+            spec_sym_to_dash += char
+    result = spec_sym_to_dash + '.html'
+    return result
 
 
 def download(link, output):
