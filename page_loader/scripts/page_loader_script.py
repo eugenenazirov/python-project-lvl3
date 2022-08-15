ddf124f370217cbe19498d007321929705f40f70
Евгений Назиров <eugenenazirov@MacBook-Air-Evgenij.local>
2022-08-15 23:22:15 +0600

    Fix linter issues

--- page_loader/scripts/page_loader_script.py
+++ page_loader/scripts/page_loader_script.py
@@ -4,8 +4,8 @@ import os
 
 
 def main():
-    pgloader = argparse.ArgumentParser(prog='page-loader', description='Downloads\
-        HTML pages on your machine.')
+    pgloader = argparse.ArgumentParser(prog='page-loader', 
+    description='Downloads HTML pages on your machine.')
     pgloader.add_argument('url')
     pgloader.add_argument('-o', '--output', help='path to output html file')
     args = pgloader.parse_args()
