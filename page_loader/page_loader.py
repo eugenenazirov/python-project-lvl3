dcdedbf4541946022de31b729aaf3db0750d1ae5
Евгений Назиров <eugenenazirov@MacBook-Air-Evgenij.local>
2022-08-16 14:08:39 +0600

    Add image local saving & tests for it

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from bs4 import BeautifulSoup
 
 
 # tmp_dir = tempfile.TemporaryDirectory()
@@ -27,10 +28,58 @@ def make_file_name(link: str):
     return result
 
 
+def get_img_tags(src_soup):
+    return src_soup.find_all('img')
+
+
+def save_img(link, img, path, name):
+    dir_name = os.path.splitext(name)[0] + '_files'
+    dir_path = os.path.join(path, dir_name)
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+    assets_path = os.path.join(dir_path, 'assets')
+    if not os.path.exists(assets_path):
+        os.mkdir(assets_path)
+    local_name = os.path.split(link)[1]
+    local_full_path = os.path.join(assets_path, local_name)
+    local_tag_path = os.path.join(dir_name, 'assets', local_name)
+    with open(local_full_path, 'w') as f:
+        f.write(img)
+    return local_tag_path
+
+
+def download_images(img_tags, output_path, name):
+    old_and_new_links = []
+    for tag in img_tags:
+        link = tag.get('src')
+        img_text = requests.get(link).text
+        path_to_img = save_img(link, img_text, output_path, name)
+        old_and_new_links.append((link, path_to_img))
+    return old_and_new_links
+
+
+def change_image_links(html: str, links: list):
+    for link_pair in links:
+        src_link = link_pair[0]
+        local_link = link_pair[1]
+        if src_link in html:
+            html = html.replace(src_link, local_link)
+    return html
+
+
+def make_soup(html_file):
+    return BeautifulSoup(html_file, 'html.parser')
+
+
 def download(link, output):
     src = requests.get(link).text
     filename = make_file_name(link)
+    soup = make_soup(src)
+    src_img_tags = get_img_tags(soup)
+    web_and_local_links = download_images(src_img_tags, output, filename)
+    soup_html = soup.prettify()
+    html_localized_img = change_image_links(soup_html, web_and_local_links)
     output_path = os.path.join(output, filename)
     with open(output_path, 'w+') as output_file:
-        output_file.write(src)
+        output_file.write(html_localized_img)
     return output_path
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,12 +1,19 @@
 import pytest
 import os
 import tempfile
+import re
+from base64 import b64encode
 from page_loader import download
 
 
 @pytest.fixture
-def download_link():
+def download_url():
     return 'http://test.com/example'
+
+
+@pytest.fixture
+def get_img_url():
+    return "https://test.com/img/image.png"
 
 
 @pytest.fixture
@@ -14,20 +21,69 @@ def expected_name():
     return 'test-com-example.html'
 
 
-def test_download_path(requests_mock, download_link, tmp_path, expected_name):
+@pytest.fixture
+def test_image_html():
+    path_to_html = os.path.join('tests', 'fixtures', 'img_page.html')
+    with open(path_to_html, 'r') as f:
+        html = f.read()
+    return html
+
+
+def test_download_path(requests_mock, download_url, expected_name):
     tmp_dir = tempfile.TemporaryDirectory()
     tmp_path = tmp_dir.name
-    requests_mock.get(download_link, text='<p>example</p>')
+    requests_mock.get(download_url, text='<p>example</p>')
     assertion_result = os.path.join(tmp_path, expected_name)
-    assert download(download_link, tmp_path) == assertion_result
+    assert download(download_url, tmp_path) == assertion_result
 
 
-def test_request_with_mock(requests_mock, download_link, tmp_path):
+def test_request_with_mock(requests_mock, download_url):
     tmp_dir = tempfile.TemporaryDirectory()
     tmp_path = tmp_dir.name
-    requests_mock.get(download_link, text='<p>example</p>')
+    requests_mock.get(download_url, text='<p>example</p>')
     assertion_result = '<p>example</p>'
-    downloaded_file_path = download(download_link, tmp_path)
+    downloaded_file_path = download(download_url, tmp_path)
     with open(downloaded_file_path, 'r') as d_file:
         downloaded_file = d_file.read()
-    assert downloaded_file == assertion_result
+    minified_downloaded_file = re.sub(r"\s+|\n|\r|\s+$", '', downloaded_file)
+    assert minified_downloaded_file == assertion_result
+
+
+def test_image_download_path(requests_mock, download_url, get_img_url, test_image_html):
+    tmp_dir = tempfile.TemporaryDirectory()
+    tmp_path = tmp_dir.name
+    test_img_path = os.path.join('tests', 'fixtures', 'img', 'image.png')
+    requests_mock.get(download_url, text=test_image_html)
+    with open(test_img_path, 'r') as t_img:
+        test_img = t_img
+    requests_mock.get(get_img_url, body=test_img)
+    assertion_result = os.path.exists(test_img_path)
+    downloaded_page = download(download_url, tmp_path)
+    page_dir = os.path.splitext(downloaded_page)[0] + '_files'
+    downloaded_img_path = os.path.join(page_dir, 'assets', 'image.png')
+    is_img_downloaded = os.path.exists(downloaded_img_path)
+    assert is_img_downloaded == assertion_result
+
+
+def test_html_changing(requests_mock, download_url, get_img_url, test_image_html):
+    tmp_dir = tempfile.TemporaryDirectory()
+    tmp_path = tmp_dir.name
+
+    test_img_path = os.path.join('tests', 'fixtures', 'img', 'image.png')
+    requests_mock.get(download_url, text=test_image_html)
+    with open(test_img_path, 'r') as t_img:
+        test_img = t_img
+    requests_mock.get(get_img_url, body=test_img)
+
+    downloaded_page = download(download_url, tmp_path)
+    page_dir = os.path.splitext(downloaded_page)[0] + '_files'
+    page_dir_name = os.path.split(page_dir)[1]
+
+    downloaded_img_path = os.path.join(page_dir, 'assets', 'image.png')
+    local_name = os.path.split(downloaded_img_path)[1]
+    local_tag_path = os.path.join(page_dir_name, 'assets', local_name)
+
+    with open(downloaded_page, 'r') as d_html:
+        html = d_html.read()
+
+    assert local_tag_path in html
