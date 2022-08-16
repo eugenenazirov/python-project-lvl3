c3e6547b41805f36917c88ade5af282df33f5952
Евгений Назиров <eugenenazirov@MacBook-Air-Evgenij.local>
2022-08-16 15:23:49 +0600

    Refactor downloading images process

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -32,7 +32,17 @@ def get_img_tags(src_soup):
     return src_soup.find_all('img')
 
 
-def save_img(link, img, path, name):
+def is_link_local(link):
+    return not any((link.startswith('http'), link.startswith('https')))
+
+
+def make_full_link(src_link, img_link):
+    site = os.path.split(src_link)[0]
+    return site + '/' + img_link
+
+
+def save_img(link, path, name):
+    img_content = requests.get(link).content
     dir_name = os.path.splitext(name)[0] + '_files'
     dir_path = os.path.join(path, dir_name)
     if not os.path.exists(dir_path):
@@ -43,18 +53,21 @@ def save_img(link, img, path, name):
     local_name = os.path.split(link)[1]
     local_full_path = os.path.join(assets_path, local_name)
     local_tag_path = os.path.join(dir_name, 'assets', local_name)
-    with open(local_full_path, 'w') as f:
-        f.write(img)
+    with open(local_full_path, 'wb') as f:
+        f.write(img_content)
     return local_tag_path
 
 
-def download_images(img_tags, output_path, name):
+def download_images(src_link, img_tags, output_path, name):
     old_and_new_links = []
     for tag in img_tags:
-        link = tag.get('src')
-        img_text = requests.get(link).text
-        path_to_img = save_img(link, img_text, output_path, name)
-        old_and_new_links.append((link, path_to_img))
+        img_link = tag.get('src')
+        if img_link:
+            if is_link_local(img_link):
+                img_link_original = img_link
+                img_link = make_full_link(src_link, img_link)
+            path_to_img = save_img(img_link, output_path, name)
+            old_and_new_links.append((img_link_original, path_to_img))
     return old_and_new_links
 
 
@@ -72,11 +85,17 @@ def make_soup(html_file):
 
 
 def download(link, output):
-    src = requests.get(link).text
+    headers = {
+        "accept": "*/*",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
+             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 \
+                Safari/537.36"
+    }
+    src = requests.get(link, headers=headers).text
     filename = make_file_name(link)
     soup = make_soup(src)
     src_img_tags = get_img_tags(soup)
-    web_and_local_links = download_images(src_img_tags, output, filename)
+    web_and_local_links = download_images(link, src_img_tags, output, filename)
     soup_html = soup.prettify()
     html_localized_img = change_image_links(soup_html, web_and_local_links)
     output_path = os.path.join(output, filename)
