efa563cf8d0dd56b86cc5def87b47a383f22ba59
Евгений Назиров <eugenenazirov@MacBook-Air-Evgenij.local>
2022-08-22 13:15:36 +0600

    Add link tag download
    Dowloading local resources as img and link will be unified.

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -2,15 +2,27 @@ from page_loader import pl
 import os
 
 
-def download(link, output):
-    src = pl.get_html_page(link)
-    filename = pl.make_file_name(link)
+def download(url, output):
+    src = pl.get_html_page(url)
+    filename = pl.make_file_name(url)
     soup = pl.make_soup(src)
-    src_img_tags = pl.get_img_tags(soup)
-    web_local_links = pl.download_images(link, src_img_tags, output, filename)
     soup_html = soup.prettify()
-    html_localized_img = pl.change_image_links(soup_html, web_local_links)
+
+    params = {
+        'src_link': url,
+        'output_path': output,
+        'name': filename
+    }
+
+    src_img_tags = pl.get_img_tags(soup)
+    web_and_local_links = []
+    web_and_local_links.extend(pl.download_resource(src_img_tags, params))
+
+    src_link_tags = pl.get_link_tags(soup)
+    web_and_local_links.extend(pl.download_resource(src_link_tags, params))
+
+    localized_html = pl.change_links(soup_html, web_and_local_links)
     output_path = os.path.join(output, filename)
     with open(output_path, 'w+') as output_file:
-        output_file.write(html_localized_img)
+        output_file.write(localized_html)
     return output_path
--- page_loader/pl.py
+++ page_loader/pl.py
@@ -1,6 +1,8 @@
 import requests
 import os
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from urllib.parse import urljoin
 
 
 headers = {
@@ -10,17 +12,89 @@ headers = {
                 Safari/537.36"}
 
 
+def make_soup(html_file):
+    return BeautifulSoup(html_file, 'html.parser')
+
+
 def get_html_page(link):
     return requests.get(link, headers=headers).text
 
 
-def make_file_name(link: str):
-    root_ext = os.path.splitext(link)
+def get_img_tags(src_soup):
+    return src_soup.find_all('img')
+
+
+def has_protocol(link: str):
+    return any((link.startswith('http'), link.startswith('https')))
+
+
+def is_local(src_url: str, resource_url: str):
+    src_host = urlparse(src_url).hostname
+    resource_host = urlparse(resource_url).hostname
+    same_hosts = src_host == resource_host
+    doesnt_have_protocol = not has_protocol(resource_url)
+    return same_hosts or doesnt_have_protocol
+
+
+def make_full_link(src_url, img_link):
+    site = os.path.split(src_url)[0]
+    return urljoin(site, img_link)
+
+
+def save_img(url, path, name):
+    img_content = requests.get(url).content
+    dir_name = os.path.splitext(name)[0] + '_files'
+    dir_path = os.path.join(path, dir_name)
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+    assets_path = os.path.join(dir_path, 'assets')
+    if not os.path.exists(assets_path):
+        os.mkdir(assets_path)
+    local_name = os.path.split(url)[1]
+    local_full_path = os.path.join(assets_path, local_name)
+    local_tag_path = os.path.join(dir_name, 'assets', local_name)
+    with open(local_full_path, 'wb') as f:
+        f.write(img_content)
+    return local_tag_path
+
+
+def process_tag(tag, params):
+    if tag.name == ('link'):
+        resource_link = tag.get('href')
+    else:
+        resource_link = tag.get('src')
+
+    src_link = params.get('src_link')
+    output_path = params.get('output_path')
+    name = params.get('name')
+
+    if not all((resource_link, is_local(
+            src_url=src_link,
+            resource_url=resource_link))):
+        return
+
+    original_link = resource_link
+    if not has_protocol(resource_link):
+        resource_link = make_full_link(src_link, resource_link)
+    path_to_img = save_resource(resource_link, output_path, name)
+
+    return original_link, path_to_img
+
+
+def download_resource(img_tags, params):
+    old_and_new_links = []
+    for tag in img_tags:
+        old_and_new_links.append(process_tag(tag, params=params))
+    return old_and_new_links
+
+
+def make_file_name(url: str, file_ext='.html'):
+    root_ext = os.path.splitext(url)
     parse_link = root_ext[0]
-    parse_link = link.split('/')
+    parse_link = url.split('/')
     parse_link.pop(0)
     parse_link.pop(0)
-    if link.endswith('.html'):
+    if url.endswith(file_ext):
         link_end = os.path.splitext(parse_link[-1])
         parse_link.pop(-1)
         parse_link.append(link_end[0])
@@ -31,73 +105,40 @@ def make_file_name(link: str):
             spec_sym_to_dash += '-'
         else:
             spec_sym_to_dash += char
-    result = spec_sym_to_dash + '.html'
+    result = spec_sym_to_dash + file_ext
     return result
 
 
-def get_img_tags(src_soup):
-    return src_soup.find_all('img')
-
-
-def is_link_local(link):
-    return not any((link.startswith('http'), link.startswith('https')))
-
-
-def make_full_link(src_link, img_link):
-    site = os.path.split(src_link)[0]
-    return site + '/' + img_link
-
-
-def process_img_tag(tag, params):
-    img_link = tag.get('src')
-    src_link = params.get('src_link')
-    output_path = params.get('output_path')
-    name = params.get('name')
-    if img_link:
-        img_link_original = img_link
-        if is_link_local(img_link):
-            img_link = make_full_link(src_link, img_link)
-        path_to_img = save_img(img_link, output_path, name)
-    return img_link_original, path_to_img
-
-
-def save_img(link, path, name):
-    img_content = requests.get(link).content
-    dir_name = os.path.splitext(name)[0] + '_files'
-    dir_path = os.path.join(path, dir_name)
-    if not os.path.exists(dir_path):
-        os.mkdir(dir_path)
-    assets_path = os.path.join(dir_path, 'assets')
-    if not os.path.exists(assets_path):
-        os.mkdir(assets_path)
-    local_name = os.path.split(link)[1]
-    local_full_path = os.path.join(assets_path, local_name)
-    local_tag_path = os.path.join(dir_name, 'assets', local_name)
-    with open(local_full_path, 'wb') as f:
-        f.write(img_content)
-    return local_tag_path
-
-
-def download_images(src_link, img_tags, output_path, name):
-    old_and_new_links = []
-    params = {
-        'src_link': src_link,
-        'output_path': output_path,
-        'name': name
-    }
-    for tag in img_tags:
-        old_and_new_links.append(process_img_tag(tag, params=params))
-    return old_and_new_links
-
-
-def change_image_links(html: str, links: list):
+def change_links(html: str, links: list):
     for link_pair in links:
-        src_link = link_pair[0]
-        local_link = link_pair[1]
+        if link_pair:
+            src_link = link_pair[0]
+            local_link = link_pair[1]
         if src_link in html:
             html = html.replace(src_link, local_link)
     return html
 
 
-def make_soup(html_file):
-    return BeautifulSoup(html_file, 'html.parser')
+def get_link_tags(src_soup):
+    return src_soup.find_all('link')
+
+
+def save_resource(url, path, name):
+    content = requests.get(url).content
+    dir_name = os.path.splitext(name)[0] + '_files'
+    dir_path = os.path.join(path, dir_name)
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+
+    file_ext = os.path.splitext(url)[1]
+    if file_ext:
+        local_name = make_file_name(url, file_ext=file_ext)
+    else:
+        local_name = make_file_name(url)
+
+    local_full_path = os.path.join(dir_path, local_name)
+    local_tag_path = os.path.join(dir_name, local_name)
+
+    with open(local_full_path, 'wb') as f:
+        f.write(content)
+    return local_tag_path
