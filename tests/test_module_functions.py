dd54881da9b8479998b1d9e7a2e6546d880ce69f
Евгений Назиров <eugenenazirov@MacBook-Air-Evgenij.local>
2022-08-22 14:04:24 +0600

    Add new unit tests

---
+++ tests/test_module_functions.py
@@ -0,0 +1,40 @@
+import pytest
+import os
+from page_loader import pl
+from bs4 import BeautifulSoup
+import requests
+
+
+@pytest.fixture
+def html():
+    return '<p>example</p>'
+
+
+@pytest.fixture
+def url():
+    return 'http://test.com/example'
+
+
+def test_headers():
+    assert all((pl.headers, isinstance(pl.headers, dict)))
+
+
+def test_make_soup(html):
+    soup = pl.make_soup(html)
+    result = all((soup,
+    isinstance(soup, BeautifulSoup)
+    ))
+    assert result
+
+
+def test_get_html_page(requests_mock, url, html):
+    requests_mock.get(url, text=html)
+    text = pl.get_html_page(url)
+    result = all((text, isinstance(text, str)))
+    assert result
+
+
+def test_get_tags(html):
+    soup = pl.make_soup(html)
+    tags = pl.get_tags(soup, 'p')
+    result = all((tags, isinstance(tags, list)))
